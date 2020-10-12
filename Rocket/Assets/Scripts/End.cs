cebb20ebc93e9ac6b734418de211cb7b86a9fd6d
Janssens Brent (s106999) <brent.janssens@student.ap.be>
2020-10-12 11:12:08 +0200

    Initial Commit

---
+++ Rocket/Assets/Scripts/End.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class End : MonoBehaviour
+{
+    private string end = "You Won!";
+
+    public TextMeshProUGUI textCoins;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        textCoins.text = end.ToString();
+    }
+}
---
+++ Rocket/Assets/Scripts/RocketMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+
+    float rotationLeftRight = 0;
+    float rotationForwardBackward = 0;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("space key was pressed");
+            transform.Translate(0, 0.03f, 0);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotationLeftRight -= 0.001f;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotationLeftRight += 0.001f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            rotationLeftRight = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            rotationLeftRight = 0;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rotationForwardBackward += 0.001f;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotationForwardBackward -= 0.001f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            rotationForwardBackward = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            rotationForwardBackward = 0;
+        }
+
+
+        transform.Rotate(rotationLeftRight, 0, rotationForwardBackward, Space.Self);
+    }
+}
